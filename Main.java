474f239dfd23824314b9f8d79e16efa2037adf71
Carlos Alves <carlosalves91@edu.unifor.br>
2022-10-27 09:47:32 -0300

    Adicionando classe lista e nó

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Teste de implementação de lista");
+        Lista lista=new Lista();
+        lista.adiciona(1);
+        lista.adiciona(2);
+        lista.adiciona(3);
+        lista.adiciona(4);
+        lista.adiciona(5);
+        System.out.println(lista);
     }
 }
