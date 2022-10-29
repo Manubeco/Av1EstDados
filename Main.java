b51ace26e9ac3580bf80fd0fef686b53aaae7907
Manubeco <emanuelbeco@outlook.com>
2022-10-29 09:18:08 -0300

    Fechando scanner

--- Main.java
+++ Main.java
@@ -77,6 +77,7 @@ public static void main(String[] args) {
                 }
             }
         }
+        entrada.close();
     }
 }
 //                    System.out.println("Teste de implementação de lista");
